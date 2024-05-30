636c966f8aa5a32c1e897392ecb480bcd2e446c2
Geraxxx5 <gerax.pineda@gmail.com>
2024-05-29 20:43:13 -0600

    feat: new objects visibility

--- portafolio-presentacion/src/pages/index.jsx
+++ portafolio-presentacion/src/pages/index.jsx
@@ -1,15 +1,12 @@
-import { Inter } from "next/font/google";
 import styles from "@styles/Home.module.css";
 import { useState, useEffect, useRef } from "react";
 import Popup from "@components/popup";
 import Switch from "@components/Switch";
 import Paw from "@components/Paw";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   const [position, setPosition] = useState({ x: 0, y: 0 })
-  const [color, setColor] = useState("Transparent") //Transparent, black
+  const [color, setColor] = useState("black") //Transparent, black
   const [isVisible, setIsVisible] = useState(false)
   const [changePosition, setChangePosition] = useState(false)
   const childRef = useRef(null);
@@ -17,6 +14,8 @@ export default function Home() {
   const [countOn, setCountON] = useState(0)
   const [rotateHand, setRotateHand] = useState("Rotate(-90deg)")
   const [animate, setAnimate] = useState(false)
+  const [objectVisible, setObjectVisible] = useState(true)
+  const [expand, setExpand] = useState(false)
 
   const handleMouseMove = (event) => {
     setPosition({ x: event.clientX, y: event.clientY });
@@ -56,6 +55,12 @@ export default function Home() {
     }else{
       setRotateHand("Rotate(90deg)")
       setAnimate(true)
+      setTimeout(() => {
+        setExpand(true)
+      },1000)
+      setTimeout(() => {
+        setObjectVisible(false)
+      },1000)
     }
   }
 
@@ -76,19 +81,22 @@ export default function Home() {
         height: "100vh", 
         width: "100vw",
         backgroundColor:"purple",
-        position: 'relative' 
+        position: 'relative', 
+        overflowX:'auto'
       }}
       onMouseMove={handleMouseMove}
     >
-      <Popup text={"Que alguien encienda las luces"} isVisible={isVisible}></Popup>
-      <div 
+      {objectVisible && (<Popup text={"Que alguien encienda las luces"} isVisible={isVisible}></Popup>)}
+      {objectVisible && (<div 
         className={styles.lightEffect}
         style={{
           background: `radial-gradient(circle 200px at ${position.x}px ${position.y}px, transparent, ${color})`
         }}
-      />
-      <Switch onClick={onStart} isVis={changePosition} animar={animate}/>
-      {destroy == false ? (<Paw ref={childRef} height={"80%"} top={"40%"} seconds={1}/>): (<Paw ref={childRef} istranslate={true} rotate={rotateHand} height={"600%"} top={"150%"} seconds={0.8} />)}
+      />)}
+      {objectVisible && (
+        <Switch onClick={onStart} isVis={changePosition} animar={animate}/>
+      )}
+      {objectVisible == true ? destroy == false ? (<Paw ref={childRef} height={"80%"} top={"40%"} seconds={1}/>): (<Paw ref={childRef} istranslate={true} rotate={rotateHand} height={"600%"} top={"150%"} seconds={0.8} />):(<div></div>)}
       <div className={`${styles.aparecerTexto} ${animate ? styles.aparecer : ''}`}>
         <div className={styles.textoNombre}>Gerardo Pineda</div>
         <div className={styles.textoNombre} style={{fontSize:"50px"}}>Full stack developer</div>
